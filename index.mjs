2f4576e395f4c81c7d7c4cb16a7925ad4afc78e9
hollali <dheztinykartel@gmail.com>
2024-01-19 18:27:02 +0000

    Add event listener to add button to log input value to console.

--- index.mjs
+++ index.mjs
@@ -15,7 +15,8 @@ const shoppingListInDB = ref (database, "shoppingList")
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
+
 addButtonEl.addEventListener("click" , function () {
     let inputValue = inputFieldEl.value
     console.log(inputValue)
-})
+});
